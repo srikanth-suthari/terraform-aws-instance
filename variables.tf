4994495aab637f7f0382ba7228906bb8f1d9c187
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-09 22:38:20 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "tags" {
+    type = map
+    default = {}    # It means no default tags are declared, we can also override this as our requirments.
+}
