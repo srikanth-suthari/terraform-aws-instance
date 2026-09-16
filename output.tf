a2bed7c36ee790db22c0e476aeaeee87d2b68644
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-16 07:07:10 +0530

    terraform

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "ec2_private_ip" {
     value = aws_instance.this.private_ip
 }
 
-output "ec2_instance_id" {
-    value = aws_instance.this.instance_id
+output "ec2_instance_type" {
+    value = aws_instance.this.instance_type
 }
