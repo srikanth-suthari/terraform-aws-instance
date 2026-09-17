b9825f99be299d456d12840dbf640720fec6ea53
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-17 22:10:40 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,6 @@ resource "aws_instance" "this" {
     ami = var.ami_id # mandatory
     instance_type = var.instance_type # mandatory
     # vpc_security_group_ids = var.sg_ids # mandatory
+    #Name will be taken from the cli
     tags = var.tags # optional
 }
